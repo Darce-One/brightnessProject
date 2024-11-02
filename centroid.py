bcd3bdd2adcbb2e823e8c529ddfc3ac24cac4ce7
Darce-One <doucy8@gmail.com>
2024-11-02 13:10:24 +0100

    semi-working

--- centroid.py
+++ centroid.py
@@ -4,33 +4,65 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
+import soundfile as sf
+
+def get_centroid_avg(audio_array: np.ndarray, fs: int) -> float:
+    centroid = librosa.feature.spectral_centroid(y=audio_array, sr=fs)
+    avg_centroid = centroid.mean()
+    return avg_centroid
+
+def apply_bandpass(audio_array: np.ndarray, band_width: float, fs: int) -> np.ndarray:
+    # find the initial centroid - this will need to be the centroid after bandpassing!
+    target_centroid = get_centroid_avg(audio_array, fs)
+
+    # create the bandpass filter and apply it initially
+    bp_instance = ess.BandPass(bandwidth=band_width, cutoffFrequency=target_centroid, sampleRate=fs)
+    bp_audio = bp_instance(audio_array)
+
+    # get the actual centroid after bandpass
+    actual_centroid = get_centroid_avg(bp_audio, fs)
+    print(f'actual_centroid: {actual_centroid}')
+
+    # set up gradient descent
+    test_centroid = target_centroid
+    iter = 0
+    # while abs(target_centroid - actual_centroid) > 2 or iter:
+    for iter in range(10):
+        # gradient descent on test_centoid using abs(target_centroid - actual_centroid) as the cost function
+        test_centroid = test_centroid + 0.2 * (target_centroid - actual_centroid) # check that is correct
+        bp_instance = ess.BandPass(bandwidth=band_width, cutoffFrequency=test_centroid, sampleRate=fs)
+        bp_audio = bp_instance(audio_array)
+        actual_centroid = get_centroid_avg(bp_audio, fs)
+        print(f'actual_centroid: {actual_centroid}')
+    return bp_audio
+
+def get_mag_spec(audio_array: np.ndarray)->np.ndarray:
+    return np.abs(fft(audio_array))
+
+def synthesise_sine(time_in_sec: float, fs: int, frequency: float, amplitude: float) -> np.ndarray:
+    time_in_samples = int(fs * time_in_sec)
+    time_axis = np.arange(time_in_samples) / fs
+    sinetone = amplitude * np.sin(2 * np.pi * frequency * time_axis)
+    return sinetone
 
 def main():
     # input sounds
-    x, fs = librosa.load('sounds/kick_drum.wav', mono=True, sr=44100)
+    audio_array, fs = librosa.load('sounds/oboe-A4.wav', mono=True, sr=44100)
+    fs = int(fs)
 
-    time_in_sec = 2.0
-    time_in_samples = int(fs * time_in_sec)
-
-    time_axis = np.arange(time_in_samples) / fs
-    sinetone = 0.8 * np.sin(2 * np.pi * 1000 * time_axis)
-
-    # get spectral centroid
-    # Spectrum_model = ess.Spectrum(size=N)
-    # windowing = ess.Windowing(type='blackmanharris62', zeroPadding=N)
-    # Centroid_model = ess.Centroid()
-    # spectrum = Spectrum_model(windowing(sinetone))
-    # centroid = Centroid_model(spectrum)
-
-    # centroid = librosa.feature.spectral_centroid(y=x, sr=fs, n_fft=x.size)
-    mX = np.abs(fft(x))
-    centroid = librosa.feature.spectral_centroid(y=sinetone, sr=fs)
-
-    print(centroid)
-    plt.plot((np.arange(N + 1) * fs / N), spectrum)
-    plt.show()
+    centroid_avg = get_centroid_avg(audio_array, fs)
+    print(f'centroid average = {centroid_avg}')
 
     # apply bandpass
+    bandwidth = 200
+    bp_audio = apply_bandpass(audio_array, band_width=bandwidth, fs=fs)
+    sf.write('sounds/outs/bp_oboe-A4.wav', bp_audio, fs)
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
